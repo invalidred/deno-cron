b31ba20f22b50abc5c69df1eb874ac25d8440b2d
Abdul Khan <abdulkhan@Abduls-Mac-mini.local>
2024-12-08 13:15:18 -0500

    feat: initial commit

---
+++ main.ts
@@ -0,0 +1,11 @@
+import { CronJob } from "npm:cron";
+
+
+const cron = CronJob.from({
+  cronTime: "*/5 * * * * *",
+  onTick: function () {
+    console.log(`Hello World!!! ${new Date()}`)
+  },
+});
+
+cron.start();
